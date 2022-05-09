b14ef9e1c59b4dd75530fa18964052761c547d2b
lky8967 <lky8967@gmail.com>
2022-05-09 22:10:40 +0900

    update

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 9ba8e79f059cd87345aa46da9fd3bd9d120dcb6b
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
 import certifi
-<<<<<<< HEAD
-import mongopy
+
 
 ca = certifi.where()
-=======
-ca = certifi.where()
 import mongopy
->>>>>>> 9ba8e79f059cd87345aa46da9fd3bd9d120dcb6b
+
 
 client = MongoClient(mongopy.password, tlsCAFile=ca)
 db = client.dbsparta
 
-
-<<<<<<< HEAD
 
 
 
@@ -29,11 +21,6 @@ db = client.dbsparta
 def home():
     return render_template('main.html')
 
-
-=======
-@app.route('/')
-def created():
-    return render_template('created.html')
 
 @app.route("/freezer_created", methods=["POST"])
 def freezer_created():
@@ -48,15 +35,10 @@ def freezer_created():
 
     db.freezer.insert_one(doc)
     return jsonify({'msg':'저장완료 !'})
->>>>>>> 9ba8e79f059cd87345aa46da9fd3bd9d120dcb6b
+
 
 
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run('0.0.0.0', port=5000, debug=True)
-
-=======
-    app.run('0.0.0.0', port=5000, debug=True)
->>>>>>> 9ba8e79f059cd87345aa46da9fd3bd9d120dcb6b
