90a4a723f8bb979c32507705a04bd6dac7ed62c5
lky8967 <lky8967@gmail.com>
2022-05-09 15:53:14 +0900

    got ignore

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print('test')
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+import mongopy
+
+client = MongoClient(mongopy.password, tlsCAFile=ca)
+db = client.dbsparta
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
