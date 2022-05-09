c5b9b35c45f39cda3814b6facae51205c3b04c4e
lky8967 <lky8967@gmail.com>
2022-05-09 15:05:23 +0900

    111

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print('test')
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.dhiik.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+
+
+
+
